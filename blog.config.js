bd8a15d33517e3e8ec5c22416f17c946e2fb4e03
gtygo <gtydoit@gmail.com>
2023-04-09 11:54:20 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -3,11 +3,11 @@ const BLOG = {
   author: 'gtygo',
   email: '',
   link: 'https://www.gtygo.com',
-  description: 'Accepted.',
-  lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  description: '',
+  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
-  font: 'serif', // ['sans-serif', 'serif']
+  font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
